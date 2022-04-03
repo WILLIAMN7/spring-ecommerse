63a33b02b347e141ef3b1951bfe29b1a78cf1da7
unknown <william7mna13@gmail.com>
2022-04-03 15:40:44 -0500

    crear clases model, mapeo clases a tablas, integrado diseño y vistas

--- src/main/java/com/curso/ecommerce/SpringEcommerceApplication.java
+++ src/main/java/com/curso/ecommerce/SpringEcommerceApplication.java
@@ -2,9 +2,10 @@ package com.curso.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+//import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
+//@ComponentScan(basePackages= {"com.curso.ecommerce.controller"})
 public class SpringEcommerceApplication {
 
 	public static void main(String[] args) {

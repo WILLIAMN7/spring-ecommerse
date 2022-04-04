f8014cffa25ade7ff8aa00612c5cfbbe4d0daf14
unknown <william7mna13@gmail.com>
2022-04-04 14:48:31 -0500

    ver usuarios

--- src/main/java/com/curso/ecommerce/controller/AdministradorController.java
+++ src/main/java/com/curso/ecommerce/controller/AdministradorController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.curso.ecommerce.model.Producto;
 import com.curso.ecommerce.service.IProductoService;
+import com.curso.ecommerce.service.IUsuarioService;
 
 @Controller
 @RequestMapping("/administrador")
@@ -17,12 +18,20 @@ public class AdministradorController {
 	
 	@Autowired
 	private IProductoService productoService;
+	@Autowired
+	private IUsuarioService usuarioService;
 	
 	@GetMapping("")
 	public String home(Model model) {
 		List<Producto> productos=productoService.findAll();
 		model.addAttribute("productos", productos);
 		return "administrador/home";
+	}
+	
+	@GetMapping("/usuarios")
+	public String usuarios(Model model) {
+		model.addAttribute("usuarios", usuarioService.findAll());
+		return "administrador/usuarios";
 	}
 
 }

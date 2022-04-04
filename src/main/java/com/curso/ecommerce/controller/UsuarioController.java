ae8179c71b5396b0b2812f21970e20fbf164f4ba
unknown <william7mna13@gmail.com>
2022-04-04 12:26:45 -0500

    obtener usuario login y sesion

--- src/main/java/com/curso/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/curso/ecommerce/controller/UsuarioController.java
@@ -1,5 +1,10 @@
 package com.curso.ecommerce.controller;
 
+import java.util.Optional;
+
+import javax.servlet.http.HttpSession;
+
+import org.apache.logging.log4j.spi.LoggerRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +35,31 @@ public class UsuarioController {
 		logger.info("usuario registro : {}", usuario);
 		usuario.setTipo("USER");
 		usuarioService.save(usuario);
+		return "redirect:/";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "usuario/login";
+	}
+	
+	@PostMapping("/acceder")
+	public String acceder(Usuario usuario, HttpSession session) {
+		logger.info("accesos : {}", usuario);
+		
+		Optional<Usuario> user = usuarioService.findByEmail(usuario.getEmail());
+		logger.info("Usuario de db: {}", user.get());
+		if(user.isPresent()) {
+			session.setAttribute("idusuario", user.get().getId());
+			if(user.get().getTipo().equals("ADMIN")) {
+				return "redirect:/administrador";				
+			}else {
+				return "redirect:/";
+			}
+		}else {
+			logger.info("usuario no existe");
+		}
+		
 		return "redirect:/";
 	}
 }
--- src/main/java/com/curso/ecommerce/service/UsuarioServiceImpl.java
+++ src/main/java/com/curso/ecommerce/service/UsuarioServiceImpl.java
@@ -23,6 +23,11 @@ public class UsuarioServiceImpl implements IUsuarioService{
 		return usuarioRepository.save(usuario);
 	}
 
+	@Override
+	public Optional<Usuario> findByEmail(String email) {
+		return usuarioRepository.findByEmail(email);
+	}
+
 	
 
 }
